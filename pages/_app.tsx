5f377b9bd730800f8a2e49908bb94216cf2eea08
masaki_yamanaka <ki.ymnka9@gmail.com>
2021-11-06 19:20:14 +0900

    feat: :+1: avoid multiple redirect uri error #4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,22 @@ import Amplify from 'aws-amplify'
 import config from '../src/aws-exports'
 Amplify.configure(config)
 
+if (typeof window !== "undefined") {
+  const { host } = window.location;
+  if (config.oauth.redirectSignIn.includes(',')) {
+    const filterHost = (url: string) => new URL(url).host === host;
+    config.oauth.redirectSignIn = config.oauth.redirectSignIn
+      .split(',')
+      .filter(filterHost)
+      .shift()!;
+    config.oauth.redirectSignOut = config.oauth.redirectSignOut
+      .split(',')
+      .filter(filterHost)
+      .shift()!;
+  }
+}
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
