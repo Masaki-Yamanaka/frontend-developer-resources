73b622fb919ddf55b030d57be013951751cb3f80
crypt0box <pray.happy76@gmail.com>
2021-12-16 18:08:52 +0900

    feat: implement tentative create user feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import Amplify from 'aws-amplify'
+import Amplify, { API, graphqlOperation }  from 'aws-amplify'
+import Auth from '@aws-amplify/auth'
 import config from '../aws-exports'
+import { createUser } from "../graphql/mutations"
 Amplify.configure(config)
 
 if (typeof window !== 'undefined') {
@@ -14,6 +17,23 @@ if (typeof window !== 'undefined') {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const createUserField = async () => {
+      const user = await Auth.currentAuthenticatedUser()
+      const userInfo = {
+        id: user.username,
+        name: user.attributes.name,
+        email: user.attributes.email,
+        profileImagePath: user.attributes.picture,
+        progressRate: 0,
+        resourcesCount: 0,
+      }
+      await API.graphql(graphqlOperation(createUser, {input: userInfo}))
+      console.log('ユーザーの作成に成功しました');
+    }
+    createUserField()
+  }, [])
+
   return <Component {...pageProps} />
 }
 export default MyApp
