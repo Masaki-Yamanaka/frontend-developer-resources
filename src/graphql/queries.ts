52f052e8acc53f24b3b1f19b3f8bb6690f1628cd
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-12-19 10:22:09 +0900

    feat/ResourceUserのschema作成

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -16,6 +16,9 @@ export const getResource = /* GraphQL */ `
         createdAt
         updatedAt
       }
+      users {
+        nextToken
+      }
       createdAt
       updatedAt
     }
@@ -51,6 +54,9 @@ export const getUser = /* GraphQL */ `
       progressRate
       resourcesCount
       posts {
+        nextToken
+      }
+      resources {
         nextToken
       }
       createdAt
