f9628323f6d1923bac0cb19469d6ac28423d0982
crypt0box <pray.happy76@gmail.com>
2022-01-09 21:31:11 +0900

    feat: implement pagelink ui

--- src/components/model/sidebar/SideBar.tsx
+++ src/components/model/sidebar/SideBar.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react'
+import { useRouter } from 'next/router'
 import Auth from '@aws-amplify/auth'
+import { IconContext } from 'react-icons'
+import { AiOutlineHome, AiOutlineCheckCircle } from 'react-icons/Ai'
+import { FiLogOut } from 'react-icons/Fi'
 import styles from './SideBar.module.scss'
 import { SidebarUserProfile } from './SidebarUserProfile'
 import { SidebarProgressRate } from './SidebarProgressRate'
@@ -8,6 +12,8 @@ import { Button } from '@/src/components/ui/Button'
 import { useCurrentUser } from '@/src/components/hooks/useCurrentUser'
 
 export const SideBar = ({ children }: { children: ReactNode }) => {
+  const router = useRouter()
+  const location = router.pathname
   const { currentUser } = useCurrentUser()
   return (
     <>
@@ -16,10 +22,23 @@ export const SideBar = ({ children }: { children: ReactNode }) => {
           <SidebarUserProfile currentUser={currentUser?.getUser} />
           <SidebarProgressRate currentUser={currentUser?.getUser} />
           <div className={styles.linkContainer}>
-            <PageLink href='/'>Home</PageLink>
-            <PageLink href='/resource'>CheckList</PageLink>
-            <PageLink href='/login'>Login</PageLink>
-            <Button onClick={() => Auth.signOut()}>LogOut</Button>
+            <IconContext.Provider value={{ size: '24px' }}>
+              <PageLink className={`${styles.link} ${location === '/' ? styles.isActive : ''}`} href='/'>
+                <AiOutlineHome />
+                <span className={styles.linkName}>Home</span>
+              </PageLink>
+              <PageLink
+                className={`${styles.link} ${location === '/resource' ? styles.isActive : ''}`}
+                href='/resource'
+              >
+                <AiOutlineCheckCircle />
+                <span className={styles.linkName}>CheckList</span>
+              </PageLink>
+              <Button className={styles.link} onClick={() => Auth.signOut()}>
+                <FiLogOut />
+                <span className={styles.linkName}>LogOut</span>
+              </Button>
+            </IconContext.Provider>
           </div>
         </section>
         <article className={styles.main}>{children}</article>
